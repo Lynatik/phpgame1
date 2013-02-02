65235f9f439646e75aeb561539ed6614d4c4d2cd
akahito <chmodakahito@gmail.com>
2013-02-02 22:36:32 +0200

    create table area

--- database.sql
+++ database.sql
@@ -28,6 +28,14 @@ CREATE TABLE `account` (
 	PRIMARY KEY(`id`)
 );
 
+DROP TABLE IF EXISTS `area`;
+CREATE TABLE `area` (
+	`id` TINYINT UNSIGNED NOT NULL,
+	`name` VARCHAR(20) NOT NULL,
+	`about` VARCHAR(255) NOT NULL DEFAULT "",
+	`enable_pvp` BOOLEAN NOT NULL DEFAULT TRUE,
+	PRIMARY KEY(`id`)
+);
 
 DROP TABLE IF EXISTS `level_info`;
 CREATE TABLE `level_info` (
@@ -36,5 +44,3 @@ CREATE TABLE `level_info` (
 	`skills` TINYINT UNSIGNED NOT NULL DEFAULT 3,
 	PRIMARY KEY(`lvl`)
 );
-
--- тестовое изминение
