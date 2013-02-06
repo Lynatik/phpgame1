56852a84bce72598e077eb218230a4c8023a897a
akahito <chmodakahito@gmail.com>
2013-02-06 10:55:12 +0100

    Удаление лишних файлов 2

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,10 +4,9 @@ class Main extends CI_Controller {
 	public function index() {
 		$this->db->where('is_online', TRUE);
 		$this->db->from('account');
-		$online = $this->db->count_all_results();
 
-		$this->load->view('main', array(
-			'online' => $online
-		));
+		$this->data['online'] = $this->db->count_all_results();
+
+		$this->load->view('main', $this->data);
 	}
 }
